7636f20c3897bf5f0ecabe5b1c4e9e191fd2a1f8
yassirkalkhi <yasserkalkhi@gmail.com>
2025-12-09 23:13:14 +0100

    fix DB seed

--- node/src/seed.ts
+++ node/src/seed.ts
@@ -2,15 +2,24 @@ import dotenv from 'dotenv';
 import connectDB from './config/db';
 import { seedDatabase } from './seeders/dataSeeder';
 import { ensureAdminExists } from './seeders/adminSeeder';
+import User from './models/User';
 
 dotenv.config();
 
 const runSeed = async () => {
     try {
         await connectDB();
-        await seedDatabase();
+         const alreadySeeded = await User.countDocuments();
+
+        if (alreadySeeded > 0) {
+            console.log("Database already seeded. Skipping...");
+            return;
+        }else{
+            await seedDatabase();
+        }
+
+
         await ensureAdminExists();
-        
         process.exit(0);
     } catch (error) {
         console.error('Seeding failed:', error);
